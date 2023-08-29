7e852e6a29b3da8b9edf219b8e48adf248491aa6
AquaXenze <93196370+aquaxenze@users.noreply.github.com>
2023-08-29 11:08:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,23 +12,23 @@
     <link rel="stylesheet" href="budget.css?v=0">
     <!-- *** JQuery *** -->
     <!-- ONLINE -->
-    <!-- <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script> -->
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     <!-- OFFLINE -->
-    <script src="jquery-3.7.0.min.js"></script>
+<!--     <script src="jquery-3.7.0.min.js"></script> -->
 
     <!-- *** Bootstrap *** -->
     <!-- ONLINE -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <!-- OFFLINE -->
-    <link rel="stylesheet" href="bootstrap-5.3.1/css/bootstrap.min.css">
-    <script src="bootstrap-5.3.1/js/bootstrap.min.js"></script>
+<!--     <link rel="stylesheet" href="bootstrap-5.3.1/css/bootstrap.min.css"> -->
+<!--     <script src="bootstrap-5.3.1/js/bootstrap.min.js"></script> -->
 
     <!-- *** FONTAWESOME *** -->
     <!-- ONLINE -->
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- OFFLINE -->
-    <link rel="stylesheet" href="fontawesome/css/all.min.css" />
+<!--     <link rel="stylesheet" href="fontawesome/css/all.min.css" /> -->
 
 
     <!-- *** COOKIES JS *** -->
